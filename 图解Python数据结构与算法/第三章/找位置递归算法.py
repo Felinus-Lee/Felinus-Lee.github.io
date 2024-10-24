00a441432b809e20e0a78ee85ab44e19ce2cd3c8
李福临 <108742622+Felinus-Lee@users.noreply.github.com>
2024-10-24 15:25:37 +0800

    Delete 图解Python数据结构与算法/第三章 directory

--- 图解Python数据结构与算法/第三章/找位置递归算法.py
+++
@@ -1,8 +0,0 @@
-def find_seat_recursive(current_row):
-    if current_row == 1:
-        return 1  # 第一排的人是确定的，排数为1
-    else:
-        # 向前方询问，递归调用
-        previous_row = find_seat_recursive(current_row - 1)
-        print(f"前一排的人员说他们在第 {previous_row} 排")
-        return previous_row + 1  # 在前一排的基础上加1，得到当前排数
