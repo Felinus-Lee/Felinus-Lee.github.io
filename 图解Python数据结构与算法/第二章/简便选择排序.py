7e1cf405ada976c419dce6a47c052a7237fbe642
李福临 <108742622+Felinus-Lee@users.noreply.github.com>
2024-10-24 15:23:18 +0800

    Delete 图解Python数据结构与算法/第二章 directory

--- 图解Python数据结构与算法/第二章/简便选择排序.py
+++
@@ -1,8 +0,0 @@
-def select_sort_simple(li):
-    li_new=[]
-    for i in range(len(li)):
-        min_val=min(li)
-        li_new.append(min_val)
-        li.remove(min_val)
-        print(li_new)
-    return li_new
